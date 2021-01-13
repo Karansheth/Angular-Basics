cb7bf035377109f2fc161953de854f9fe77cd9f3
Sheth <karan.sheth@thomsonreuters.com>
2021-01-13 15:30:40 +0530

    Some Files Deleted

--- TextWrap/src/app/text-wrap.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextWrapPipe } from './text-wrap.pipe';
-
-describe('TextWrapPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TextWrapPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
