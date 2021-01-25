3e22a7e87672fd7d47bdb852989d77b85f0af679
Karansheth <karan.sheth148@gmail.com>
2021-01-25 13:17:59 +0530

    Unit Testing

--- Product-Details-Using-Angular-Service/src/app/products-information/products-information.component.spec.ts
+++ Product-Details-Using-Angular-Service/src/app/products-information/products-information.component.spec.ts
@@ -1,20 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
+import { PostService } from '../Services/post.service';
 
 import { ProductsInformationComponent } from './products-information.component';
 
 describe('ProductsInformationComponent', () => {
   let component: ProductsInformationComponent;
   let fixture: ComponentFixture<ProductsInformationComponent>;
+  let services: PostService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductsInformationComponent ]
+      declarations: [ ProductsInformationComponent ],
+      imports: [HttpClientModule],
+      providers: [PostService]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductsInformationComponent);
+    services = TestBed.inject(PostService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -22,4 +29,12 @@ describe('ProductsInformationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('test ngOnInit', () => {
+    let obj: any;
+    spyOn(services, 'getProducts').and.returnValue(obj);
+    component.ngOnInit();
+    expect(component).toBeTruthy();
+  });
+
 });
